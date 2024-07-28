51fc2db2df4ff5e0a1a3244780241e84c33b5501
Darryl Rachel <racheldarryl@icloud.com>
2024-07-28 15:32:51 -0700

    done: Voting.sol, working: test

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -1,2 +1,22 @@
 const Voting = artifacts.require("Voting");
 
+contract("Voting", accounts => {
+  let votingInstance;
+
+  before(async () => {
+    votingInstance = await Voting.deployed();
+  });
+
+  it("should initialize with zero candidates", async () => {
+    const count = await votingInstance.candidatesCount();
+    assert.equal(count, 0, "Candidates count should be zero");
+  });
+
+  it("should allow a user to vote for a candidate", async () => {
+    await votingInstance.addCandidate("Batman", { from: acounts[0] });
+    const candidate = await votingInstance.getCandidate(0);
+    assert.equal(candidate[0], "Batman", "Canadidate name should be Batman");
+    assert.equal(candidate[1].toNumber(), 0, "Candidate vote count should be zero");
+  })
+
+})
